cc255686f63e24bd6ad8ca155f28d5b21dca4af8
arun <nuranisafirdausy1201@gmail.com>
2022-03-22 14:32:05 +0700

    first commit

---
+++ lib/pages/home_page.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class HomePage extends StatelessWidget {
+Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
---
+++ lib/pages/item_page.dart
@@ -0,0 +1,7 @@
+import 'package:flutter/material.dart';
+
+class ItemPage extends StatelessWidget {
+Widget build(BuildContext context) {
+    throw UnimplementedError();
+  }
+}
