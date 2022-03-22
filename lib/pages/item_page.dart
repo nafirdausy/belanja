20d33c06447a062832f4c4c298e98d80c60c5f82
arun <nuranisafirdausy1201@gmail.com>
2022-03-22 16:45:30 +0700

    praktikum 4

--- lib/pages/item_page.dart
+++ lib/pages/item_page.dart
@@ -1,7 +1,18 @@
 import 'package:flutter/material.dart';
+import 'package:belanja/models/item.dart';
 
 class ItemPage extends StatelessWidget {
+  
+@override
 Widget build(BuildContext context) {
-    throw UnimplementedError();
+    final args = ModalRoute.of(context)!.settings.arguments as Item;
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("Shopping List"),
+      ),
+      body: Center(
+        child: Text(args.name+" with "+ args.price.toString()),
+      ),
+    );
   }
-}
+} 
